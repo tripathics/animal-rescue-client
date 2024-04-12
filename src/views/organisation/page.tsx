a652a10f566bc5340b3f9dc5392f988160e67dca
Daknya Bam <daknyabam18@gmail.com>
2024-04-12 20:36:53 +0530

    icons

--- src/views/organisation/page.tsx
+++ src/views/organisation/page.tsx
@@ -3,11 +3,10 @@ import PageHeader from "../../components/layouts/PageHeader/PageHeader";
 import styles from "./organisation.module.scss";
 import cx from "classnames";
 
-
 const dummyOrganizations = [
     {
         id: 1,
-        image: "https://via.placeholder.com/150",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWScwFdBwyd3X1wmw57yx0uOIaHgKRMneJKf9az1zLLg&s",
         name: "sharda stray foundation",
         url: "https://example.com",
         location: "Kathmandu, Nepal",
@@ -15,7 +14,7 @@ const dummyOrganizations = [
     },
     {
         id: 2,
-        image: "https://via.placeholder.com/150",
+        image: "https://dynamic.brandcrowd.com/asset/logo/3a3f54e6-3f66-47cd-90c6-32fc7c92fcbf/large?logoTemplateVersion=1&v=638439085781441630",
         name: "Purucat House",
         url: "https://example.com",
         location: "tamilnadu, india",
@@ -24,7 +23,32 @@ const dummyOrganizations = [
     },
     {
         id: 3,
-        image: "https://via.placeholder.com/150",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-kTnBeoUCgUnKjCZ5zJVcqmw5a4k8G33o3hvma2_85A&s",
+        name: "Chandrashaker gau shala",
+        url: "https://example.com",
+        location: "koyom batur, indonesia",
+        established: " 31 feb ,2010",
+    },
+    {
+        id: 4,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeHZbYSXoWT8oGLBXzjWPk-NlID7XJYtCV7j_yOYiLNQ&s",
+        name: "sharda stray foundation",
+        url: "https://example.com",
+        location: "Kathmandu, Nepal",
+        established: " 42 jan ,2015",
+    },
+    {
+        id: 5,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQksP8KGtG4jZviU3GaWLQVc600H_xi28dC-A&s",
+        name: "Purucat House",
+        url: "https://example.com",
+        location: "tamilnadu, india",
+        established: "24 july ,2013",
+
+    },
+    {
+        id: 6,
+        image: "https://l450v.alamy.com/450v/2dkttc4/animal-shelter-flat-icon-color-simple-element-from-volunteering-collection-creative-animal-shelter-icon-for-web-design-templates-infographics-and-2dkttc4.jpg",
         name: "Chandrashaker gau shala",
         url: "https://example.com",
         location: "koyom batur, indonesia",
